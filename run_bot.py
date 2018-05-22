b5b90c94d01ad4a1bf66eb815dd17f0a1d4df02a
Maros Kopec <makopec@redhat.com>
2018-05-22 10:16:13 +0200

    Load subreddits from file

--- run_bot.py
+++ run_bot.py
@@ -1,4 +1,10 @@
 from src.bot import login
+import json
 
+# Read subreddits from file
+with open('subreddits.json', 'r') as file:
+    file_data = json.load(file)
+
+print(file_data['subreddits'])
 reddit = login()
 print(reddit.user.me())
